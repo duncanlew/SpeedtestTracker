d6f6c4d24487dc1ed64bad7e8430ec0754ccce09
Duncan Lew <duncanlew@users.noreply.github.com>
2024-12-08 14:24:18 +0100

    initial commit

---
+++ test/speedtest_tracker.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SpeedtestTracker from '../lib/speedtest_tracker-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/speedtest_tracker-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SpeedtestTracker.SpeedtestTrackerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
