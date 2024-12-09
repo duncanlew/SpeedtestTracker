90df59a191aa17299fd831bae71bd9b0a2a4efe1
Duncan Lew <duncanlew@users.noreply.github.com>
2024-12-09 20:36:23 +0100

    working version

--- lib/speedtest-tracker-stack.ts
+++ lib/speedtest-tracker-stack.ts
@@ -3,20 +3,29 @@ import { Construct } from 'constructs';
 import * as apigw from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as path from 'node:path';
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class SpeedtestTrackerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps){
     super(scope, id, props)
-    const fn = new lambda.Function(this, 'SaveSpeedtestFunction', {
+    // const fn = new lambda.Function(this, 'SaveSpeedtestFunction', {
+    //   runtime: lambda.Runtime.NODEJS_LATEST,
+    //   handler: 'index.handler',
+    //   code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')),
+    // });
+
+    const fn = new NodejsFunction(this, 'SaveSpeedtestFunction', {
+      entry: path.resolve(__dirname, 'lambda-handler/index.js'),
       runtime: lambda.Runtime.NODEJS_LATEST,
       handler: 'index.handler',
-      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')),
-    });
+    })
 
     const endpoint = new apigw.LambdaRestApi(this, `ApiGwEndpoint`, {
       handler: fn,
       restApiName: `SpeedtestTrackerApi`,
     });
+
+
 
   }
 }
