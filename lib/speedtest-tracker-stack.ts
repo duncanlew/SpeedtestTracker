66130116fd7f56ac77aa1b3493bfc45bf460a4a8
Duncan Lew <duncanlew@users.noreply.github.com>
2024-12-10 20:53:25 +0100

    dynamo db

--- lib/speedtest-tracker-stack.ts
+++ lib/speedtest-tracker-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as apigw from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as path from 'node:path';
 import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
 
@@ -18,6 +19,11 @@ export class SpeedtestTrackerStack extends cdk.Stack {
     const endpoint = new apigw.LambdaRestApi(this, `ApiGwEndpoint`, {
       handler: fn,
       restApiName: `SpeedtestTrackerApi`,
+    });
+
+    const table = new dynamodb.TableV2(this, 'Table', {
+      partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
+      tableName: "speedtest-tracker",
     });
 
   }
