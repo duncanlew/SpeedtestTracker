f28054671738cdfa332eae3eabafc02f701a8b71
Duncan Lew <duncanlew@users.noreply.github.com>
2024-12-10 20:33:37 +0100

    more types

--- lib/lambda-handler/index.ts
+++ lib/lambda-handler/index.ts
@@ -1,13 +1,13 @@
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
 import {APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult, Context} from "aws-lambda";
+import {Todo} from "../models/todo.interface";
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
     try {
         // Extract specific properties from the event object
         const { resource, path, httpMethod, headers, queryStringParameters, body } = event;
 
-        const axiosResponse = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-
+        const axiosResponse: AxiosResponse<Todo> = await axios.get<Todo>('https://jsonplaceholder.typicode.com/todos/1');
         const response = {
             resource,
             path,
@@ -15,7 +15,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
             headers,
             queryStringParameters,
             body,
-            todos: axiosResponse.data,
+            todo: axiosResponse.data,
         };
 
         return {
